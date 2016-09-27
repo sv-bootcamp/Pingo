eb75bef24bc4198d426127f049ba5ee557dfb118
Youngchan Je <ochanje210@gmail.com>
2016-09-27 11:12:54 +0900

    added map

---
+++ index.ios.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import App from './shared/containers/app';
+
+AppRegistry.registerComponent('Goober', () => App);
---
+++ shared/components/map.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import {StyleSheet, View} from 'react-native';
+import MapView from 'react-native-maps';
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+    },
+});
+
+export default class Map extends Component {
+    render() {
+        return (
+            <View style ={styles.container}>
+                <MapView style ={styles.map}
+                         initialRegion={{
+                             latitude: 37.78825,
+                             longitude: -122.4324,
+                             latitudeDelta: 0.0922,
+                             longitudeDelta: 0.0421,
+                         }}
+                />
+            </View>
+        );
+    }
+}
