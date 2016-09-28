c2c0b5b7ed92351bf953a90fc64cd7bb2fb62d52
yuda1124 <yuda1124@naver.com>
2016-09-28 20:29:03 +0900

    add jest code (#3)
    * add jest code
    
    * Delete map-test.js.snap

---
+++ __tests__/map-test.js
@@ -0,0 +1,13 @@
+import 'react-native';
+import React from 'react';
+import Map from '../shared/components/map';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <Map />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
