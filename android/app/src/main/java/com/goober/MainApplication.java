1f99ea40154dbdcb3f285102d7bd147a9c7f8ee0
elegantuniv <korea651@gmail.com>
2016-09-29 16:09:23 +0900

    added library as react-native-camera

--- android/app/src/main/java/com/goober/MainApplication.java
+++ android/app/src/main/java/com/goober/MainApplication.java
@@ -5,6 +5,7 @@ import android.util.Log;
 
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactApplication;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RCTCameraPackage(),
           new MapsPackage()
       );
     }
--- index.ios.js
+++ index.ios.js
@@ -2,7 +2,8 @@ import {
   AppRegistry,  
 } from 'react-native';
 
-import gb from './app/GooberProject';
+// import gb from './app/GooberProject';
+// import cv from './app/CameraView';
+import App from './shared/containers/app';
 
-
-AppRegistry.registerComponent('GooberProject', () => gb);
+AppRegistry.registerComponent('GooberProject', () => App);
