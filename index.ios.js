7c0259d1511c330daefb169157062ac86dbe5ba2
elegantuniv <korea651@gmail.com>
2016-10-11 10:57:29 +0900

    remove unused comp

--- index.ios.js
+++ index.ios.js
@@ -2,10 +2,6 @@ import {
   AppRegistry,  
 } from 'react-native';
 
-// import gb from './shared/components/GooberProject';
-// import cv from './shared/components/CameraView';
-// import App from './shared/containers/app';
-// import li from './shared/containers/listLayout';
 import cf from './shared/components/CreateForm';
 
 AppRegistry.registerComponent('GooberProject', () => cf);
