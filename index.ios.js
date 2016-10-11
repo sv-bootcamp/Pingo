2c5116541c8822275b20f626de819b2d483f555d
elegantuniv <korea651@gmail.com>
2016-10-11 11:01:36 +0900

    first Post data to server (#11)
    * added create form
    
    * binding server API briefly
    
    * set public props for communicating with location
    
    * remove unused comp

--- index.ios.js
+++ index.ios.js
@@ -2,9 +2,6 @@ import {
   AppRegistry,  
 } from 'react-native';
 
-// import gb from './shared/components/GooberProject';
-// import cv from './shared/components/CameraView';
-import App from './shared/containers/app';
-// import li from './shared/containers/listLayout';
+import cf from './shared/components/CreateForm';
 
-AppRegistry.registerComponent('GooberProject', () => App);
+AppRegistry.registerComponent('GooberProject', () => cf);
