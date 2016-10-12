49232907d6d6c06135ade9560dee0666deed73af
kyy <yuda1124@naver.com>
2016-10-12 16:58:44 +0900

    add list filtering

--- shared/reducers/list.js
+++ shared/reducers/list.js
@@ -1,18 +1,15 @@
 import * as types from '../actions/actionTypes';
 import update from 'react-addons-update';
-import { ListView } from 'react-native';
 
 const initialState = {
-  dataSource: new ListView.DataSource({
-    rowHasChanged: (r1, r2) => r1 !== r2
-  }).cloneWithRows([])
+  dataSource: []
 };
 
 const list = (state = initialState, action) => {
   switch (action.type) {
   case types.getAllItems:
     return update(state, {
-      dataSource: { $set: state.dataSource.cloneWithRows(action.items) }
+      dataSource: { $set: action.items }
     });
   default:
     return state;
