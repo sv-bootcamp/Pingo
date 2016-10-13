eab62d8221d7f946f128b8c384f5eff450910c56
kyy <yuda1124@naver.com>
2016-10-13 15:30:00 +0900

    add saved_button

--- shared/components/Card.js
+++ shared/components/Card.js
@@ -1,5 +1,5 @@
 import React, { PropTypes, Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 
 const styles = StyleSheet.create({
   title: {
@@ -10,17 +10,36 @@ const styles = StyleSheet.create({
   address: {
     flex: 1,
     fontSize: 16
+  },
+  favorite: {
+    height: 40,
+    width: 40,
   }
 });
 
 class Card extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isSaved : false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      isSaved : !this.state.isSaved
+    });
+  }
+
   render() {
     return (
 		<View>
-			<Text style={styles.title}>{this.props.title}</Text>
-			<Text style={styles.address}>{this.props.address}</Text>
-				<TouchableOpacity	style={{margin: 1, backgroundColor: 'red', flex: 1}}>
-					<Text style={styles.text}>Favorite</Text>
+			<Text style= { styles.title }>{this.props.title}</Text>
+			<Text style= { styles.address }>{this.props.address}</Text>
+				<TouchableOpacity	onPress = {this.handleClick}>
+          <Image style= {styles.favorite}
+                 source = { this.state.isSaved ? require('../../static/images/yellow_star.png') : require('../../static/images/empty_star.png')}/>
 				</TouchableOpacity>
 		</View>
 	);
