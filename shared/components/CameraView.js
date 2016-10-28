f2910e5c17266b09d6f1c14d5ca87fedb4b5f118
youngchan <ochanje210@gmail.com>
2016-10-28 17:58:49 +0900

    camera view style

--- shared/components/CameraView.js
+++ shared/components/CameraView.js
@@ -1,14 +1,13 @@
-import React, {
-  Component, PropTypes
-} from 'react';
-
+import React, {Component, PropTypes} from 'react';
 import {
   StyleSheet,
   Dimensions,
   View,
-  Text
+  Text,
+  TouchableOpacity,
+  Image
 } from 'react-native';
-import {Actions} from 'react-native-router-flux';
+import { Actions } from 'react-native-router-flux';
 import Camera from 'react-native-camera';
 
 const styles = StyleSheet.create({
@@ -30,15 +29,32 @@ const styles = StyleSheet.create({
   },
   bottom: {
     height: 573 - 360,
+    flexDirection: 'row',
     width: Dimensions.get('window').width,
     backgroundColor: '#2b2b2b',
     position: 'absolute',
     bottom: 0
+  },
+  bottom_buttons: {
+    position: 'absolute',
+    bottom: 60,
+    alignSelf: 'center',
+    backgroundColor: 'blue',
+    width: Dimensions.get('window').width
+  },
+  btn_taking_photo: {
+    alignSelf: 'center',
+    left: Dimensions.get('window').width / 2 - 90
+  },
+  btn_camera_switch: {
+    alignSelf: 'center',
+    left: 32
+  },
+  btn_flash: {
+    alignSelf: 'center',
+    left: Dimensions.get('window').width / 2 - 32
   }
 });
-
-//height: Dimensions.get('window').height,
-//width: Dimensions.get('window').width
 
 class CameraView extends Component {
   takePicture() {
@@ -54,17 +70,39 @@ class CameraView extends Component {
     return (
       <View style={styles.container}>
         <View style={styles.top}>
-          <Text> PHOOTTOO </Text>
+          <Text style={{color: 'white'}}> Photo </Text>
         </View>
         <Camera
           ref={(cam) => {
             this.camera = cam;
           }}
           style={styles.preview}
-          aspect={Camera.constants.Aspect.fill}>
-        <Text> asdfasdfsafsdafsdafs </Text>
-      </Camera>
+          aspect={Camera.constants.Aspect.fill}/>
       <View style={styles.bottom}>
+          <TouchableOpacity
+              style={styles.btn_camera_switch}
+              onPress={()=>{}}>
+            <Image
+                style={{height: 48, width: 48}}
+                source={require('../resources/camera/icon_camera_switch.png')}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.btn_taking_photo}
+            onPress={this.takePicture.bind(this)}>
+            <Image
+              style={{height: 80, width: 80}}
+              source={require('../resources/camera/btn_taking_photo.png')}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.btn_flash}
+            onPress={()=>{}}>
+            <Image
+              style={{height: 48, width: 48}}
+              source={require('../resources/camera/icon_flash.png')}
+            />
+          </TouchableOpacity>
       </View>
       </View>
     );
