c6fdd0f0ced5249b117111d177b2196701e21b69
youngchan <ochanje210@gmail.com>
2016-10-31 13:01:21 +0900

    added button handle and close

--- shared/components/CameraView.js
+++ shared/components/CameraView.js
@@ -5,7 +5,8 @@ import {
   View,
   Text,
   TouchableOpacity,
-  Image
+  Image,
+  Platform
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import Camera from 'react-native-camera';
@@ -53,6 +54,38 @@ const styles = StyleSheet.create({
   btn_flash: {
     alignSelf: 'center',
     left: Dimensions.get('window').width / 2 - 32
+  },
+  btn_again: {
+    alignSelf: 'center',
+    left: Dimensions.get('window').width / 2 - 25
+  },
+  btn_close: {
+    left: 15,
+    top: 24
+  },
+  btn_use: {
+    left: Dimensions.get('window').width - 50,
+    top: -22
+  },
+  text_top: {
+    alignSelf: 'center',
+    left: Dimensions.get('window').width / 2 - 180,
+    color: 'white',
+    fontSize: 17,
+    ...Platform.select({
+      android: {
+        fontFamily: 'Roboto-Medium'
+      }
+    })
+  },
+  text_use: {
+    color: '#2c8cff',
+    fontSize: 17,
+    ...Platform.select({
+      android: {
+        fontFamily: 'Roboto-Regular'
+      }
+    })
   }
 });
 
@@ -70,7 +103,8 @@ class CameraView extends Component {
         orientation: Camera.constants.Orientation.auto,
         flashMode: Camera.constants.FlashMode.auto
       },
-      isRecording: false
+      isRecording: false,
+      Done: false
     };
   }
 
@@ -78,13 +112,35 @@ class CameraView extends Component {
     this.camera.capture()
     .then((data) => {
       this.props.setCurrentPic(data.path);
-      Actions.createForm({type: 'replace'});
+      this.setState({
+        Done: true
+      });
+      Actions.refresh();
     })
     .catch(err => console.error(err));
   }
 
+  takeAgain() {
+    this.setState({
+      Done: false
+    });
+    Actions.refresh();
+  }
+
   handleFlash() {
     //todo: handle camera flash here
+  }
+
+  handleClose() {
+    this.props.setCurrentScene('map');
+    Actions.pop();
+  }
+
+  handleUse() {
+    this.setState({
+      Done: false
+    });
+    Actions.createForm({type: 'replace'});
   }
 
   switchType() {
@@ -109,50 +165,85 @@ class CameraView extends Component {
     return (
       <View style={styles.container}>
         <View style={styles.top}>
-          <Text style={{color: 'white'}}> Photo </Text>
-        </View>
-        <Camera
-          ref={(cam) => {
-            this.camera = cam;
-          }}
-          style={styles.preview}
-          type={this.state.camera.type}
-          aspect={Camera.constants.Aspect.fill}
-          flashMode={this.state.camera.flashMode}
-        />
-      <View style={styles.bottom}>
           <TouchableOpacity
+            style={styles.btn_close}
+            onPress={this.handleClose.bind(this)}>
+            <Image
+              style={{height:24, width: 24}}
+              source={require('../resources/camera/btn_close.png')}
+            />
+          </TouchableOpacity>
+          <Text style={styles.text_top}> Photo </Text>
+          {(this.state.Done === true) ?
+            <TouchableOpacity
+              style={styles.btn_use}
+              onPress={this.handleUse.bind(this)}>
+              <Text style={styles.text_use}> Use </Text>
+            </TouchableOpacity>
+            : null
+          }
+        </View>
+        {(this.state.Done === true) ?
+          <Image source={{uri: this.props.pic}} style={styles.preview} />
+            :
+          <Camera
+            ref={(cam) => {
+              this.camera = cam;
+            }}
+            style={styles.preview}
+            type={this.state.camera.type}
+            aspect={Camera.constants.Aspect.fill}
+            flashMode={this.state.camera.flashMode}
+          />
+        }
+        {(this.state.Done === true) ?
+          <View style={styles.bottom}>
+            <TouchableOpacity
+              style={styles.btn_again}
+              onPress={this.takeAgain.bind(this)}>
+              <Image
+                style={{height: 48, width: 48}}
+                source={require('../resources/camera/icon_camera_again.png')}
+              />
+            </TouchableOpacity>
+          </View>
+          :
+          <View style={styles.bottom}>
+            <TouchableOpacity
               style={styles.btn_camera_switch}
               onPress={this.switchType.bind(this)}>
-            <Image
+              <Image
                 style={{height: 48, width: 48}}
                 source={require('../resources/camera/icon_camera_switch.png')}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.btn_taking_photo}
-            onPress={this.takePicture.bind(this)}>
-            <Image
-              style={{height: 80, width: 80}}
-              source={require('../resources/camera/btn_taking_photo.png')}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.btn_flash}
-            onPress={this.handleFlash.bind(this)}>
-            <Image
-              style={{height: 48, width: 48}}
-              source={require('../resources/camera/icon_flash.png')}
-            />
-          </TouchableOpacity>
-      </View>
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.btn_taking_photo}
+              onPress={this.takePicture.bind(this)}>
+              <Image
+                style={{height: 80, width: 80}}
+                source={require('../resources/camera/btn_taking_photo.png')}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.btn_flash}
+              onPress={this.handleFlash.bind(this)}>
+              <Image
+                style={{height: 48, width: 48}}
+                source={require('../resources/camera/icon_flash.png')}
+              />
+            </TouchableOpacity>
+          </View>
+        }
       </View>
     );
   }
 }
 
 CameraView.propTypes = {
-  setCurrentPic: PropTypes.func
+  setCurrentPic: PropTypes.func,
+  setCurrentScene: PropTypes.func,
+  pic: PropTypes.string
 };
 
 export default CameraView;
