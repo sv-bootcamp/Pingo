e1d9b112b9600d009000d1ddab527144ffae09f6
Youngchan Je <ochanje210@gmail.com>
2016-11-01 17:33:36 +0900

    simpler convertMonth function

--- shared/components/Create.js
+++ shared/components/Create.js
@@ -272,21 +272,8 @@ class Create extends Component {
   }
 
   convertMonth(m) {
-    switch (m) {
-    case '1': return 'Jan.';
-    case '2': return 'Feb.';
-    case '3': return 'Mar.';
-    case '4': return 'Apr.';
-    case '5': return 'May';
-    case '6': return 'Jun.';
-    case '7': return 'Jul.';
-    case '8': return 'Aug.';
-    case '9': return 'Sept.';
-    case '10': return 'Oct.';
-    case '11': return 'Nov.';
-    case '12': return 'Dec.';
-    default: return '';
-    }
+    const month = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'Jun.', 'Jul.', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.'];
+    return month[m-'1'];
   }
 
   handleOnDateChangeStart(datetime) {
