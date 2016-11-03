6a5803cf24a973d99b6c1669f3ecf9092a69e4a6
Youngchan Je <ochanje210@gmail.com>
2016-11-03 19:11:26 +0900

    following my page style

--- shared/components/myPage.js
+++ shared/components/myPage.js
@@ -3,11 +3,39 @@ import SmallHeader from './smallHeader';
 import {
   View,
   Image,
-  Text
+  Text,
+  StyleSheet,
+  Platform
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
 import ImgBtnSetting from '../resources/smallHeader/btnSetting.png';
+
+const styles = StyleSheet.create({
+  myPageTextUserName: {
+    position: 'absolute',
+    bottom: 0,
+    fontSize: 19,
+    color: '#2b2b2b'
+  },
+  myPageTextLogInFacebook: {
+    color: '#2c8cff'
+  },
+  fontRobotoRegular: {
+    ...Platform.select({
+      android: {
+        fontFamily: 'Roboto-Regular'
+      }
+    })
+  },
+  fontRobotoMedium: {
+    ...Platform.select({
+      android: {
+        fontFamily: 'Roboto-Medium'
+      }
+    })
+  }
+});
 
 class MyPage extends Component {
   constructor(props) {
@@ -25,20 +53,61 @@ class MyPage extends Component {
     this.props.setCurrentScene('map');
     Actions.map({type: 'replace'});
   }
+
+  handleButtonSetting() {
+    // todo
+  }
+
+  renderUserBox() {
+    return (
+      <View style={{backgroundColor: 'white', flexDirection: 'column', flex: 136, elevation: 1}}>
+        <View style={{backgroundColor: 'white', flex: 32}}/>
+        <View style={{flexDirection: 'row', flex: 72}}>
+          {this.renderUserPicture()}
+          {this.renderTextBoxInUserBox()}
+          <View style={{backgroundColor: 'white', flex: 24}}/>
+        </View>
+        <View style={{backgroundColor: 'white', flex: 32}}/>
+      </View>
+    );
+  }
+  renderUserPicture() {
+    return (
+      <View style={{flexDirection: 'row', flex: 24 + 72 + 16}}>
+        <View style={{flex: 24}}/>
+        <View style={{flex: 88}}>
+          <Text>Picture</Text>
+        </View>
+      </View>
+    );
+  }
+
+  renderTextBoxInUserBox() {
+    return (
+      <View style={{flexDirection: 'column', flex: 225}}>
+        <View style={{flex: 35}}>
+          <Text style={[styles.myPageTextUserName, styles.fontRobotoMedium]}>Guest</Text>
+        </View>
+        <View style={{flex: 8}}/>
+        <View style={{flex: 28}}>
+          <Text style={[styles.myPageTextLogInFacebook, styles.fontRobotoRegular]}>Log in with Facebook</Text>
+        </View>
+      </View>
+    );
+  }
+
   render() {
     return (
       <View style={{flexDirection: 'column', flex: 1}}>
         <SmallHeader
           btnRight={this.renderImageButtonSetting()}
           handleBtnLeft={this.handleButtonPrev.bind(this)}
-          handleBtnRight={()=>{}}
+          handleBtnRight={this.handleButtonSetting.bind(this)}
           headerText={'myPage'}
         />
-        <View style={{backgroundColor: 'blue', flex: 1}}>
-          <Text> testing </Text>
-        </View>
-        <View style={{backgroundColor: 'black', flex: 1}}>
-          <Text> testing </Text>
+        {this.renderUserBox()}
+        <View style={{backgroundColor: 'white', flex: 440}}>
+          <Text> tab view </Text>
         </View>
       </View>
     );
