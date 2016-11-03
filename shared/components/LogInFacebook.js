d129d2700a6d6eeb6b5d7599b6dd5a313be9e645
Youngchan Je <ochanje210@gmail.com>
2016-11-03 22:57:04 +0900

    fixed indentation

--- shared/components/LogInFacebook.js
+++ shared/components/LogInFacebook.js
@@ -2,28 +2,28 @@ import React, { Component } from 'react';
 import {FBLogin, FBLoginManager} from 'react-native-facebook-login';
 
 class LogInFacebook extends Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return (
-			<FBLogin
-				ref={(fbLogin) => { this.fbLogin = fbLogin }}
-				loginBehavior={FBLoginManager.LoginBehaviors.Native}
-				permissions={["email","user_friends"]}
-				onLogin={function(e){console.log(e)}}
-				onLoginFound={function(e){console.log(e)}}
-				onLoginNotFound={function(e){console.log(e)}}
-				onLogout={function(e){console.log(e)}}
-				onCancel={function(e){console.log(e)}}
-				onPermissionsMissing={function(e){console.log(e)}}
-			/>
-		);
-	}
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <FBLogin
+        ref={(fbLogin) => { this.fbLogin = fbLogin }}
+        loginBehavior={FBLoginManager.LoginBehaviors.Native}
+        permissions={["email","user_friends"]}
+        onLogin={function(e){console.log(e)}}
+        onLoginFound={function(e){console.log(e)}}
+        onLoginNotFound={function(e){console.log(e)}}
+        onLogout={function(e){console.log(e)}}
+        onCancel={function(e){console.log(e)}}
+        onPermissionsMissing={function(e){console.log(e)}}
+      />
+    );
+  }
 }
 
 LogInFacebook.propTypes = {
-	// TBD
+  // TBD
 };
 
 export default LogInFacebook;
