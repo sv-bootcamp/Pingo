cb3f98d5aa6040aeb8ab586595742fc7eec48285
yuda1124 <yuda1124@naver.com>
2016-11-08 11:48:34 +0900

    Detail view style (#88)
    * simple detailview
    
    * simple detail view style

--- shared/components/detailHeader.js
+++ shared/components/detailHeader.js
@@ -1,6 +1,44 @@
 import React, { PropTypes, Component } from 'react';
-import { View, Image, Text, TouchableHighlight } from 'react-native';
+import { View, Image, Text, TouchableHighlight, Platform } from 'react-native';
 import {Actions} from 'react-native-router-flux';
+
+const styles = {
+  wrapper: {
+    flex: 1,
+    flexDirection : 'row',
+    backgroundColor: '#f8f8f8',
+    borderBottomWidth: 1,
+    borderBottomColor: '#e7e7e7'
+  },
+  btn_close: {
+    flex: 55,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  info: {
+    flex: 210,
+    justifyContent: 'center'
+  },
+  btn_more: {
+    flex: 24,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  btn_star: {
+    flex: 24,
+    alignItems: 'flex-start',
+    justifyContent: 'center'
+  },
+  title: {
+    ...Platform.select({
+      android: {
+        fontFamily: 'Roboto-Medium'
+      }
+    }),
+    fontSize: 14,
+    fontWeight: 'bold'
+  }
+};
 
 export default class DetailHeader extends Component {
 
@@ -10,12 +48,8 @@ export default class DetailHeader extends Component {
 
   render() {
     return (
-      <View>
-        <View>
-          <Text> {this.props.title}</Text>
-          {(this.props.date) ? <Text> {this.props.date} </Text> : null}
-        </View>
-        <View>
+      <View style = {styles.wrapper}>
+        <View style = {styles.btn_close}>
           <TouchableHighlight
             onPress = {()=>{
               this.props.setCurrentScene(this.props.lastScene);
@@ -24,6 +58,22 @@ export default class DetailHeader extends Component {
             <Image source = {require('../resources/btn_close/drawable-mdpi/btn_close.png')}/>
           </TouchableHighlight>
         </View>
+        <View style = {styles.info}>
+          <Text style = {styles.title}> {this.props.title} </Text>
+          {(this.props.date) ? <Text> {this.props.date} </Text> : null}
+        </View>
+          <View style = {styles.btn_star}>
+              <TouchableHighlight>
+                <Image source = {require('../resources/btn_star/drawable-mdpi/btn_star.png')}/>
+              </TouchableHighlight>
+          </View>
+          <View style = {{flex: 19.2}}/>
+          <View style = {styles.btn_more}>
+              <TouchableHighlight>
+                <Image source = {require('../resources/btn_more/drawable-mdpi/btn_more.png')}/>
+              </TouchableHighlight>
+          </View>
+          <View style = {{flex: 6}}/>
       </View>
     );
   }
--- shared/components/detailView.js
+++ shared/components/detailView.js
@@ -1,90 +1,174 @@
 import React, { PropTypes, Component } from 'react';
 import Swiper from 'react-native-swiper';
 import DetailHeaderLayout from '../containers/detailHeaderLayout';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, TouchableHighlight, Platform } from 'react-native';
 
 const styles = {
   wrapper: {
-    height: 461
+    flexDirection: 'column'
   },
 
   slide: {
-    justifyContent: 'center',
-    backgroundColor: 'transparent'
+    flex: 360,
+    justifyContent: 'center'
   },
 
   caption: {
-    height: 38
+    ...Platform.select({
+      android: {
+        fontFamily: 'Roboto-Medium'
+      }
+    }),
+    fontSize: 14
   },
-
-  image: {
-    width: 360,
-    height: 360
+  name: {
+    ...Platform.select({
+      android: {
+        fontFamily: 'Roboto-Medium'
+      }
+    }),
+    fontSize: 14,
+    fontWeight: 'bold'
   },
-
+  btn_flag: {
+    flex: 38,
+    justifyContent: 'center'
+  },
   index: {
     fontSize: 14,
-    height: 14,
     color: '#8e8e8e',
-    marginTop: 14,
-    marginBottom: 17,
-    marginLeft: 162
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 };
 
-const renderPagination = (index, total) => {
-  return (
-    <View style={{
-      position: 'absolute',
-      bottom: 0,
-      height: 45,
-      width: 360,
-      borderBottomWidth: 1,
-      borderColor: '#e7e7e7'
-    }}>
-      <Text style={styles.index}>
-        {index + 1} of {total}
-      </Text>
-    </View>
-  );
-};
-
 export default class DetailView extends Component {
-
   constructor(props) {
     super(props);
+    this.state = {
+      isClicked: false
+    };
+    this.renderDate = this.renderDate.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.renderPagination = this.renderPagination.bind(this);
   }
 
   componentDidMount() {
     this._swiper.scrollBy(this.props.rowID * 1);
   }
-
+  handleClick() {
+    this.setState({isClicked: !this.state.isClicked});
+  }
+  renderPagination(index, total) {
+    if (this.state.isClicked) {
+      return (<View style ={{flex: 0.3, backgroundColor: 'black'}}/>);
+    }
+    return (
+      <View style={{
+        flexDirection: 'row',
+        position: 'absolute',
+        bottom: 0
+      }}>
+        <View style = {{flex: 32, justifyContent: 'center', alignItems: 'center'}}>
+          <TouchableHighlight>
+            <Image source = {require('../resources/arrow_left/drawable-mdpi/arrow.png')}/>
+          </TouchableHighlight>
+        </View>
+        <View style = {{flex: 296, height: 45, borderTopWidth: 1, borderTopColor: '#e7e7e7'}}>
+          <View style = {{ alignItems: 'center', flex: 1, justifyContent: 'center'}}>
+            <Text style={styles.index}>
+              {index + 1} of {total}
+            </Text>
+          </View>
+        </View>
+        <View style = {{flex: 32, justifyContent: 'center', alignItems: 'center'}}>
+          <TouchableHighlight>
+            <Image source = {require('../resources/arrow_right/drawable-mdpi/arrow.png')}/>
+          </TouchableHighlight>
+        </View>
+      </View>
+    );
+  }
+  renderDate(date) {
+    const day = 86400000;
+    const hour = 3600000;
+    const minute = 60000;
+    let gap = new Date() - new Date(date);
+    let createdTime = '';
+    (gap / day >= 1) ? createdTime += Math.ceil(gap / day) + ' days ago' :
+    (gap / hour >= 1) ? createdTime += Math.ceil(gap / hour) + ' hours ago' :
+    (gap / minute >= 1) ? createdTime += Math.ceil(gap / minute) + ' minutes ago' :
+    createdTime += 'right now';
+    return (
+      <Text> {createdTime} </Text>
+    );
+  }
   render() {
     return (
-      <View>
-        <View>
-          <DetailHeaderLayout title = {this.props.title}
-                              date = {this.props.date}
-                              lastScene = {this.props.lastScene}/>
-        </View>
-        <View style={styles.wrapper}>
-          <Swiper height = {508} ref={(swiper) => {this._swiper = swiper;}}
-            showsButtons = {false}
-            renderPagination={renderPagination}
-            paginationStyle={{
-              bottom: 0,
-              position: 'absolute'
-            }} loop = {false}>
+      <View style = {{ flex: 1, backgroundColor: '#ffffff'}}>
+      {
+        (this.state.isClicked) ? <View style = {{height: 67, backgroundColor: 'black'}}/> :
+          <View style = {{height: 67}}>
+            <DetailHeaderLayout title = {this.props.title}
+                                date = {this.props.date}
+                                lastScene = {this.props.lastScene}/>
+          </View>
+      }
 
-            {this.props.detailSource.map((value)=>{
-              return (
-                <View style={styles.slide}>
-                  <Image style = {styles.image}
-                         source = {{uri: value.url}}/>
-                       <Text style = {styles.caption}> { value.caption }</Text>
-                </View>
-              );
-            })}
+        <View style = {{flex: 573}}>
+          <Swiper ref={(swiper) => {this._swiper = swiper;}}
+            showsButtons = {false}
+            renderPagination={this.renderPagination}
+            height= {549}
+            loop = {false}>
+              {this.props.detailSource.map((value)=>{
+                return (
+                  <View style = {{flex: 1}}>
+                    {
+                      (this.state.isClicked) ? <View style = {{flex: 68, backgroundColor: 'black'}}/> :
+                      <View style = {{flex: 68, flexDirection: 'row'}}>
+                        <View style = {{flex: 16}}/>
+                        <View style = {{flex: 32}}/>
+                        <View style = {{flex: 274}}>
+                          <View style = {{flex: 16}}/>
+                          <View style = {{flex: 14}}>
+                            <Text style = {styles.name}> Name 2</Text>
+                          </View>
+                          <View style = {{flex: 4}}/>
+                          <View style = {{flex: 14}}>
+                            {this.renderDate(value.createdDate)}
+                          </View>
+                          <View style = {{flex: 16}}/>
+                        </View>
+                        <View style = {styles.btn_flag}>
+                          <TouchableHighlight>
+                            <Image source = {require('../resources/btn_flag/drawable-mdpi/btn_flag.png')}/>
+                          </TouchableHighlight>
+                        </View>
+                      </View>
+                    }
+                    <View style={styles.slide}>
+                      <TouchableHighlight style = {{flex : 1}}
+                                          onPress = {this.handleClick}>
+                        <Image source = {{uri: value.url}}
+                               style = {{flex: 1}}/>
+                      </TouchableHighlight>
+                    </View>
+                    {(this.state.isClicked) ? <View style = {{flex: 103, backgroundColor: 'black'}}/> :
+                    <View style = {{flex: 103, flexDirection : 'row'}}>
+                      <View style = {{flex: 16}}/>
+                      <View style = {{flex: 328}}>
+                        <View style = {{flex: 15}}/>
+                        <View style = {{flex: 86}}>
+                          <Text style = {styles.caption}> { value.caption } </Text>
+                        </View>
+                      </View>
+                      <View style = {{flex: 16}}/>
+                    </View>
+                    }
+                  </View>
+                );
+              })}
           </Swiper>
         </View>
       </View>
