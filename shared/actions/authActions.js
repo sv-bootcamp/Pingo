a403451bbf0363fe68b2e6ad5a6626ffde9db518
Youngchan Je <ochanje210@gmail.com>
2016-11-08 20:06:49 +0900

    fixed indent

--- shared/actions/authActions.js
+++ shared/actions/authActions.js
@@ -4,32 +4,32 @@ import { AsyncStorage } from 'react-native';
 const STORAGE_KEY = '@PingoStorage:key';
 
 export const setToken = (token) => {
-	return {
-		type: types.setToken,
-		token
-	}
+  return {
+    type: types.setToken,
+    token
+  }
 };
 
 export const getUserToken = async () => {
-	try {
-		return await AsyncStorage.getItem(STORAGE_KEY);
-	} catch (error) {
-		console.log(error.message);
-	}
+  try {
+    return await AsyncStorage.getItem(STORAGE_KEY);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 export const setUserToken = async (token) => {
-	try {
-		await AsyncStorage.setItem(STORAGE_KEY, token);
-	} catch (error) {
-		console.log(error.message);
-	}
+  try {
+    await AsyncStorage.setItem(STORAGE_KEY, token);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 export const removeUserToken = async () => {
-	try {
-		await AsyncStorage.removeItem(STORAGE_KEY);
-	} catch (error) {
-		console.log(error.message);
-	}
+  try {
+    await AsyncStorage.removeItem(STORAGE_KEY);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
