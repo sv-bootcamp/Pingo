9aed99e10938f221323c99352a66e70989241dbb
Youngchan Je <ochanje210@gmail.com>
2016-12-12 16:27:06 +0900

    fix android build (#196)

--- android/app/src/main/java/com/goober/MainApplication.java
+++ android/app/src/main/java/com/goober/MainApplication.java
@@ -3,17 +3,17 @@ package com.goober;
 import android.app.Application;
 import android.util.Log;
 
-import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactApplication;
-import com.showlocationservicesdialogbox.LocationServicesDialogBoxPackage;
-import com.magus.fblogin.FacebookLoginPackage;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.github.xfumihiro.react_native_image_to_base64.ImageToBase64Package;
+import com.rnfs.RNFSPackage;
+import com.magus.fblogin.FacebookLoginPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-import com.rnfs.RNFSPackage;
+import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
@@ -30,18 +30,23 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new LocationServicesDialogBoxPackage(),
-            new FacebookLoginPackage(),
+            new MapsPackage(),
             new ImageToBase64Package(),
-            new RCTCameraPackage(),
-          new MapsPackage(),
-              new RNFSPackage()
+            new RNFSPackage(),
+            new FacebookLoginPackage(),
+            new RCTCameraPackage()
       );
     }
   };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
-      return mReactNativeHost;
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
   }
 }
