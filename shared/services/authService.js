905dcc18531226a322cc0dff6748a05d8d686f06
Youngchan Kim <ovekyc@gmail.com>
2016-12-26 11:58:38 +0900

    Fix constatn error & lint

--- shared/services/authService.js
+++ shared/services/authService.js
@@ -2,6 +2,14 @@ import {HTTPS, SERVER_ADDR, HTTPUtil, DEFAULT_HEADERS} from '../utils';
 
 const ENDPOINT = '/api/auth';
 
+/*
+TODO : checkout ADDRESS constant below
+It does not work now, The log said something like
+cannot fetch from...
+undefinedundefined/api/auth/grant
+
+I think, The making time of constant is not guaranteed the sequence.
+*/
 const ADDRESS = {
   GRANT: `${HTTPS}${SERVER_ADDR}${ENDPOINT}/grant`,
   REFRESH: `${HTTPS}${SERVER_ADDR}${ENDPOINT}/refresh`
@@ -10,27 +18,29 @@ const ADDRESS = {
 const GRANT_TYPE = {
   ANONYMOUS: 'anonymous',
   FACEBOOK: 'facebook'
-}
+};
 
 const RESTManager = {
   grant: (body) => {
-      return HTTPUtil.post(ADDRESS.GRANT, DEFAULT_HEADERS, body);
+    const address = `${HTTPS}${SERVER_ADDR}${ENDPOINT}/grant`;
+    return HTTPUtil.post(address, DEFAULT_HEADERS, body);
   },
   grantFacebook: (facebookToken) => {
-      return RESTManager.grant({
-          grantType: GRANT_TYPE.FACEBOOK,
-          facebookToken
-      });
+    return RESTManager.grant({
+      grantType: GRANT_TYPE.FACEBOOK,
+      facebookToken
+    });
   },
   grantGuest: (userKey, userSecret) => {
-      return RESTManager.grant({
-          grantType: GRANT_TYPE.ANONYMOUS,
-          userSecret,
-          userKey
-      });
+    return RESTManager.grant({
+      grantType: GRANT_TYPE.ANONYMOUS,
+      userSecret,
+      userKey
+    });
   },
   refresh: (refreshToken) => {
-      return HTTPUtil.post(ADDRESS.REFRESH, DEFAULT_HEADERS, {refreshToken});
+    const address = `${HTTPS}${SERVER_ADDR}${ENDPOINT}/refresh`;
+    return HTTPUtil.post(address, DEFAULT_HEADERS, {refreshToken});
   }
 };
 export default RESTManager;
