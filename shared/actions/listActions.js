d505a0be3aba35ab8ce76461ed2c5641eaefb798
Kim Yu Yeong <yuda1124@naver.com>
2017-01-03 16:23:19 +0900

    fix activity card bug (#223)
    * fix activity card bug
    
    * roll back other function except getDetailImage

--- shared/actions/listActions.js
+++ shared/actions/listActions.js
@@ -1,7 +1,11 @@
 import * as types from './actionTypes';
 // import {SERVER_ADDR, ENDPOINT_IMAGE, HTTP,
 //   queryBuilder, createQueryObject} from '../utils'; We will use later
+import {queryBuilder, createQueryObject} from '../utils';
 import ItemRESTManager from '../services/itemService';
+import { getAccessToken } from './authActions';
+import { HTTPS, SERVER_ADDR, ENDPOINT_IMAGE, getAuthHeaders} from '../utils';
+
 
 export function TBD() {
   return {
@@ -22,7 +26,7 @@ export const getAllItems = (zoomLevel, lat, long) => {
   return (dispatch) => {
     return ItemRESTManager.getByArea(zoomLevel, lat, long)
       .then(json => dispatch(receiveItems(json)))
-      .catch(console.log);
+      .catch(console.log); // eslint-disable-line no-console
   };
 };
 
@@ -65,11 +69,25 @@ export const setPostedUri = (uri) => {
 
 export const getDetailImage = (key) => {
   return (dispatch) => {
-    return ItemRESTManager.get(key)
+    const queries = [];
+    queries.push(createQueryObject('item', key));
+    // const address = `${HTTP}${'SERVER_ADDR'}${ENDPOINT_IMAGE}${queryBuilder(queries)}`;
+    // const address = `https://goober.herokuapp.com/api/images/${queryBuilder(queries)}`;
+    return getAccessToken().then((accessToken) => {
+      const address = `${HTTPS}${SERVER_ADDR}${ENDPOINT_IMAGE}/${queryBuilder(queries)}`;
+      return getAuthHeaders(accessToken)
+      .then(headers => {
+        return fetch(address, {
+          method: 'GET',
+          headers
+        });
+      })
+      .then(response => response.json())
       .then(json => {
         dispatch(receiveImages(json));
         return json.values;
       })
-      .catch(console.log);
+      .catch(console.log); // eslint-disable-line no-console
+    });
   };
 };
