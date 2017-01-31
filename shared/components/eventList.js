4e37241a845dd23a664b56893aa6aab7244bfae5
Youngchan Je <ochanje210@gmail.com>
2017-01-31 13:30:05 +0900

    hide camera button on list view

--- shared/components/eventList.js
+++ shared/components/eventList.js
@@ -3,10 +3,17 @@ import { ListView, View, Dimensions, Platform } from 'react-native';
 import CardLayout from '../containers/cardLayout';
 import MapButton from './mapButton';
 import { Actions } from 'react-native-router-flux';
+import { getLoginType } from './../actions/authActions';
 
 class EventList extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      loginType: ''
+    };
+    getLoginType().then(loginType => {
+      this.state.loginType = loginType;
+    });
   }
 
   renderRowTxt(rowData) {
@@ -38,19 +45,23 @@ class EventList extends Component {
             enableEmptySections={true} />
           : null
         }
-        <MapButton
-          handleOnPress={this.handleCameraButton.bind(this)}
-          imageSource={'camera'}
-          style={Platform.OS === 'android' ? {
-            position: 'absolute', zIndex: 10, elevation: 4,
-            bottom: Dimensions.get('window').width * 16 / 360,
-            right: Dimensions.get('window').width * 16 / 360
-          } : {
-            position: 'absolute', zIndex: 10,
-            bottom: Dimensions.get('window').width * 16 / 360,
-            right: Dimensions.get('window').width * 16 / 360
-          }}
-        />
+        {(this.state.loginType === 'facebook') ?
+          <MapButton
+            handleOnPress={this.handleCameraButton.bind(this)}
+            imageSource={'camera'}
+            style={Platform.OS === 'android' ? {
+              position: 'absolute', zIndex: 10, elevation: 4,
+              bottom: Dimensions.get('window').width * 16 / 360,
+              right: Dimensions.get('window').width * 16 / 360
+            } : {
+              position: 'absolute', zIndex: 10,
+              bottom: Dimensions.get('window').width * 16 / 360,
+              right: Dimensions.get('window').width * 16 / 360
+            }}
+          />
+          :
+          null
+        }
       </View>
     );
   }
